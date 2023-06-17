8681b331bf619230d8134df38bc5d673886d5d48
jackrichter <jackrichter1@gmail.com>
2023-06-17 04:07:05 +0200

    var in code.

---
+++ src/main/java/org/advanced/java/LVTI/LVTI_Example.java
@@ -0,0 +1,11 @@
+package org.advanced.java.LVTI;
+
+public class LVTI_Example {
+    // Not Here!
+    public static void main(String[] args) {
+        // Here is ok, but they must be initialized
+        var x = 2;          // int
+        var y = 3.2;        // double
+        var z = "abc";      // String
+    }
+}
